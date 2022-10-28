db848b822975eefccb7faf8b6278a13b5c6ccc56
Rita Bradley <rita@ritabradley.dev>
2022-10-27 21:10:42 -0700

    add search results to document
    - create container for search results
    - make functions to search for shows and add result image to document

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Connected')
+const searchForm = document.querySelector('#search-form');
+const queryInput = document.querySelector('#query-input');
+
+const makeImages = async (shows) => {
+	const results = document.querySelector('#results-container')
+	for (let res of shows) {
+		const img = document.createElement('img')
+		img.src = res.show.image.medium
+		results.appendChild(img)
+	}
+}
+
+const showSearch = async () => {
+	const searchTerm = queryInput.value.trim();
+	const config = {params: {q: searchTerm}}
+	const res = await axios.get('https://api.tvmaze.com/search/shows', config);
+
+	queryInput.value = ''
+	await makeImages(res.data)
+}
+
+searchForm.addEventListener('submit', e => {
+	e.preventDefault();
+	showSearch()
+})
